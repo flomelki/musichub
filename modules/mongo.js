186a3ae6d4b56aa5822ff36b882d1811e79f106b
crazygodflo <chezwam69@hotmail.com>
2016-01-21 00:29:35 +0100

    Added 'genre' filter

--- modules/mongo.js
+++ modules/mongo.js
@@ -2,47 +2,42 @@
 var recursive = require('recursive-readdir');
 var mongoose = require('mongoose');
 var path = require('path');
-
-console.log('go on')
+var fs = require('fs');
+var mm = require('musicmetadata')
 
 var musicSchema = mongoose.Schema({
-  path : {type : String, required: true}
+  path : {type : String, required: true},
+  genre : {type : String, required: true}
 });
 
 Music = mongoose.model("Music", musicSchema);
 
 var authExt = ['.mp3', '.ogg'];
 
-// TODO : move in any library
-Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i--) {
-        if (this[i] === obj) {
-            return true;
-        }
-    }
-    return false;
-}
-
 mongoose.connect("mongodb://localhost/musicalBase", function(){
-    // Drop the DB 
-    mongoose.connection.db.dropDatabase();
+  // Drop the DB 
+  mongoose.connection.db.dropDatabase();
+  
+  recursive('musicFiles', function (err, files) {
+    files = files.filter(function(file)
+    {
+      var ext = path.extname(file);
+      return authExt.contains(ext);
+    })
     
-    recursive('musicFiles', function (err, files) {
-      files = files.filter(function(file)
-      {
-        var ext = path.extname(file);
-        return authExt.contains(ext);
-      })
+    files.forEach(function(file)
+    {
+      var filePath = file.split('\\').splice(1).join('/');
       
-      files.forEach(function(file)
-      {
-        var filePath = file.split('\\').splice(1).join('/');
-        Music.create({path:filePath}, function(err, musicFile)
+      mm(fs.createReadStream(file), function (err, metadata) {
+        if (err) throw err;
+        
+        Music.create({path:filePath, genre:metadata.genre}, function(err, musicFile)
         {
           if (err)
             console.log(err);
         });
       });
     });
+  });
 });
--- routes/musics.js
+++ routes/musics.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var util = require('util');
 var router = express.Router();
-require('../modules/mongo')
 
 // client list
 router.get('/', function(req, res) {
   Music.find(function(err, musicFileList)
   {
-    console.log(musicFileList.length)
-      res.render('index', {musicFileList : musicFileList});
+    var genreList = []
+    musicFileList.forEach(function(file)
+    {
+      if (!(genreList.contains(file.genre)))
+      {
+        genreList.push(file.genre);
+      }
+    });
+    res.render('index', {musicFileList : musicFileList, genreList:genreList});
   });
 
 });
